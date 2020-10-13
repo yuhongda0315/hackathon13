e93dd2b89daa7c114cee957d97e8d014fe161298
yuhongda <yuhongda@rongcloud.cn>
2020-10-14 01:56:24 +0800

    feat: 样式美化

--- js/ipc.js
+++ js/ipc.js
@@ -23,8 +23,51 @@
       callbacks.connected(im);
     });
   };
-  var startRTC = () => {
+  var startRTC = (config, callbacks) => {
+    var { user } = config;
+    var rongRTC = new RongRTC({
+      RongIMLib: RongIMLib
+    });
+    var { Room, Stream, StreamType } = rongRTC;
 
+    var $room = new Room({
+      id: 'line13',
+      joined: function (user) {
+      },
+      left: function (user) {
+      }
+    });
+
+    var $stream = new Stream({
+      published: function (user) {
+        $stream.subscribe(user).then((user) => {
+          callbacks.subscribed(user)
+        });
+      }
+    });
+
+    $room.join(user).then(() => {
+      $stream.get().then(function ({ mediaStream }) {
+        var _user = {
+          id: user.id,
+          stream: {
+            tag: 'LINE13',
+            type: StreamType.AUDIO_AND_VIDEO,
+            mediaStream: mediaStream
+          }
+        };
+        callbacks.published(_user)
+        $stream.publish(_user).then(() => {
+          console.log('发布成功');
+        }, error => {
+          console.log(error);
+        });
+      }, error => {
+        console.error(error);
+      });
+    }, error => {
+      console.log(error);
+    });
   };
   Thirteen.IPC = {
     startIM,
